2b57669e716717ffc6fe36dacae3f621da4f2d76
Pijus <p.pozera@gmail.com>
2023-04-09 21:24:28 +0300

    AddDonations/Edit working

--- server/app.js
+++ server/app.js
@@ -166,21 +166,21 @@ app.delete("/admin/stories/:id", (req, res) => {
     });
 });
 
-app.put("/admin/stories/:id", (req, res) => {
-    const sql = `
-        UPDATE stories
-        SET current_sum = current_sum + ?
-        WHERE id = ?
-    `;
-    params = [req.body.amount, req.params.id];
+// app.put("/admin/stories/:id", (req, res) => {
+//     const sql = `
+//         UPDATE stories
+//         SET current_sum = current_sum + ?
+//         WHERE id = ?
+//     `;
+//     params = [req.body.amount, req.params.id];
 
-    con.query(sql, params, (err) => {
-        if (err) throw err;
-        res.json({
-            msg: { text: "Sritis pakeista", type: "info" },
-        });
-    });
-});
+//     con.query(sql, params, (err) => {
+//         if (err) throw err;
+//         res.json({
+//             msg: { text: "Sritis pakeista", type: "info" },
+//         });
+//     });
+// });
 app.put("/admin/stories/:id", (req, res) => {
     const action = req.body.action;
     const id = req.params.id;
